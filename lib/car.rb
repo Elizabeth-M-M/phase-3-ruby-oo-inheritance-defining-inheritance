88b177d7ea68ea1f433cd6fb946966d4c656c5ae
Elizabeth <mwendemuthusi6@gmail.com>
2023-02-14 06:29:59 +0300

    Completed assignment

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,9 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+class Car < Vehicle
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+end
+c1= Car.new(2, 4)
+puts c1.go
